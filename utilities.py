2fb784808903a20add15b7c2a1b49d01c553eda3
Heqing Ya <normanyahq@gmail.com>
2016-04-21 13:01:45 -0400

    refactor the code

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 from parameters import *
+import random
 
 
 def logging(message):
@@ -7,3 +8,80 @@ def logging(message):
     if save_log_to_file:
         with open(log_filename, "a") as f:
             f.write(message + '\n')
+
+
+def load_data(doc_file_name, label_file_name):
+    '''
+    Load docs and labels from files, and remove the '~~~~' and
+        '<s>', '</s>' tags, re-organize them into lists:
+            docs = [doc_0, doc_1, ... doc_n]
+            doc_i = [sentence_0, sentence_1, ... sentence_n]
+            sentence_i is a string
+    return: docs: list of docs as described above,
+            labels: list of integers, each is a label of corresponding doc
+    '''
+    docs = []
+    labels = []
+    with open(doc_file_name) as f:
+        docs = f.read()
+    with open(label_file_name) as f:
+        labels = f.readlines()
+    docs = docs.split('~~~~~')
+    docs = [doc.replace('\n', '').replace('<s>', '').replace(
+        '</s>', '~~~~').split('~~~~') for doc in docs]
+    labels = [int(i) for i in labels]
+    return docs, labels
+
+
+def cross_validation(model, docs, labels, batch_num):
+    '''
+    model: class instance inherited from languagemodel
+    docs: list of documents, each document is a list of sentence
+    labels: list of integers for labels
+    batch_num: the number of group to divide the data into
+    '''
+    assert len(docs) == len(labels)
+    positive_samples = [doc for i, doc in enumerate(docs) if labels[i]]
+    negative_samples = [doc for i, doc in enumerate(docs) if not labels[i]]
+    random.shuffle(positive_samples)
+    random.shuffle(negative_samples)
+    positive_per_batch = len(positive_samples) / batch_num
+    negative_per_batch = len(negative_samples) / batch_num
+
+    # accuracy
+    average_accuracy = 0
+
+    for i in range(0, batch_num):
+        test_positive = positive_samples[
+            i * positive_per_batch: min((i + 1) * positive_per_batch, len(positive_samples))]
+        test_negative = negative_samples[
+            i * negative_per_batch: min((i + 1) * negative_per_batch, len(negative_samples))]
+        training_positive = positive_samples[:i * positive_per_batch] + \
+            positive_samples[min((i + 1) * positive_per_batch, len(positive_samples)):]
+        training_negative = negative_samples[:i * negative_per_batch] + \
+            negative_samples[min((i + 1) * negative_per_batch, len(negative_samples)):]
+        logging('Training batch %d...' % (i + 1))
+        model.train(training_positive + training_negative,
+                    [1] * len(training_positive) + [0] * len(training_negative))
+        logging('Traning done, start predicting...')
+        # True Positive, False Positive, True Negative, False Negative
+        tp = fp = tn = fn = 0.
+        for doc in test_positive:
+            if model.predict(doc):
+                tp += 1
+            else:
+                fn += 1
+        for doc in test_negative:
+            if model.predict(doc):
+                fp += 1
+            else:
+                tn += 1
+        testset_size = len(test_positive) + len(test_negative)
+        accuracy = (tp + tn) * 1. / testset_size
+        logging("Test Doc: %d\ntp=%d(%.2f%%), fp=%d(%.2f%%), tn=%d(%.2f%%), fn=%d(%.2f%%)\nAccuracy=%.2f\n"
+                % (testset_size, tp, tp * 100. / len(test_positive), fp, fp * 100. / len(test_positive),
+                   tn, tn * 100. / len(test_negative), fn, fn * 100. / len(test_negative),
+                   accuracy))
+        average_accuracy += accuracy
+    average_accuracy /= batch_num
+    logging("Average Accuracy: %.2f%%" % (average_accuracy * 100))
