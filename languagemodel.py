6d106d2ed51ff3664b2dad6a33d72f0269752fc8
Heqing Ya <normanyahq@gmail.com>
2016-04-21 01:30:46 -0400

    modified class template

--- languagemodel.py
+++ languagemodel.py
@@ -1,36 +1,49 @@
 import pickle
+from languagemodel import LanguageModel
+import random
+from utilities import logging
 
 
-class LanguageModel:
+class RandomGuess(LanguageModel):
 
-    def save_model(self):
+    def save_model(self, model_file_name):
         '''
         Save your model.
         '''
-        pass
+        logging('Saving model into file: ' + model_file_name)
 
-    def load_model(self):
+        # fill your model information here
+        t_model = {}
+
+        pickle.dump(t_model, open(model_file_name, "w"))
+
+    def load_model(self, model_file_name):
         '''
         Load your model
         '''
-        pass
+        logging('loading model from file: ' + model_file_name)
+        t_model = pickle.load(open(model_file_name))
+        # copy your model information here
 
     def __init__(self, model_file_name=None):
         '''
         some initialization, do whatever you want
         like loading corpus, parameters, etc
         '''
-        # define the filename of model to load
-        self._model_file_name = model_file_name
         pass
 
     def train(self, docs, labels):
         '''
-        sentence: a string list of docs
+        docs: a list of docs, each doc is a list of sentences,
+            '~~~~', '<s>' and '</s>' are removed
         labels: a integer list of labels,
             0: fake articles, 1: real articles
         '''
-        pass
+        logging('start training...')
+
+        # implement your training algorithm here.
+
+        logging('training done, positive_rate: %f' % self.positive_rate)
 
     def predict(self, doc):
         '''
