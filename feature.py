fb1ab7032dac2fc0ab7c33e22a093c603e2453e2
Yulan Huang <yulanh@cs.cmu.edu>
2016-04-21 18:02:45 -0400

    feature parallel

--- feature.py
+++ feature.py
@@ -3,10 +3,20 @@ from bllipparser import RerankingParser
 # from nltk.parse.bllip import BllipParser
 from utilities import *
 import numpy as np
+from multiprocessing import Pool
 
 
-def rerankign_paser_score(doc, ifmean=True, ifmax=True, ifmin=True):
-	rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=True)
+rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=True)
+
+def get_parser_score(sent):
+	best = rrp.parse(sent)[0]
+	score = best.parser_score
+	tree_len = len(best.ptb_parse.tokens())
+	score /= tree_len
+
+	return score
+
+def reranking_parser_score(doc, ifmean=True, ifmax=True, ifmin=True):
 	# from nltk.data import find
 	# model_dir = find('models/bllip_wsj_no_aux').path
 	# rrp = BllipParser.from_unified_model_dir(model_dir)
@@ -15,41 +25,36 @@ def rerankign_paser_score(doc, ifmean=True, ifmax=True, ifmin=True):
 	curMax = -np.inf
 	curMin = np.inf
 	# print len(doc)
-	
+
+	# q = Queue()
+	p_list = []
+
 	logging("Start parsing doc")
-	for sent in doc:
-		
-		best = rrp.parse(sent)[0]
-		score = best.parser_score
-		tree_len = len(best.ptb_parse.tokens())
-		score /= tree_len
 
-		curTotal += score
-
-		if score > curMax:
-			curMax = score
-
-		if score < curMin:
-			curMin = score
-
-	curMean = curTotal / len(doc)
+	p = Pool(100)
+	scores = p.map(get_parser_score, doc)
+	logging(scores)
 
 	res = []
 	if ifmean:
-		res.append(curMean)
+		mean = np.sum(scores) / len(doc)
+		res.append(mean)
+
 	if ifmax:
-		res.append(curMax)
+		res.append(max(scores))
+
 	if ifmin:
-		res.append(curMin)
+		res.append(min(scores))
 
 	return res
 
 
 
 def extract_feature(doc):
-	rerankign_paser_score(doc)
+	res = reranking_parser_score(doc)
+	logging(str(res))
 
 if __name__ == '__main__':
 	docs, labels = load_data("data/train_text.txt", "data/train_label.txt")
 	for doc in docs:
-		rerankign_paser_score(doc)
+		extract_feature(doc)
