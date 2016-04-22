c3ae5281d5abbb1aa60b8df86a38c328acefbac5
Yulan Huang <yulanh@cs.cmu.edu>
2016-04-21 22:08:56 -0400

    add perplexity

--- ngrammodel.py
+++ ngrammodel.py
@@ -65,14 +65,14 @@ def get_unigram_log_prob(unigram_count):
 	total_count = count_dict_val(unigram_count)
 	unigram_prob = {}
 	for k in unigram_count:
-		unigram_prob[k] = np.log(unigram_count[k]) - np.log(total_count)
+		unigram_prob[k] = np.log2(unigram_count[k]) - np.log2(total_count)
 	print "calculated unigram prob ... "
 	return unigram_prob
 
 def get_bigram_conditional_log_prob(bigram_count, unigram_count):
 	bigram_prob = {}
 	for w1, w2 in bigram_count:
-		bigram_prob[(w1, w2)] = np.log(bigram_count[(w1, w2)]) - np.log(unigram_count[w1])
+		bigram_prob[(w1, w2)] = np.log2(bigram_count[(w1, w2)]) - np.log2(unigram_count[w1])
 	print "calculated bigram conditional prob ... "
 	return bigram_prob
 
@@ -86,20 +86,36 @@ def get_skipgram_conditional_log_prob(trigram_count):
 
 	skipgram_prob = {}
 	for (w1, w2, w3) in trigram_count:
-		skipgram_prob[(w1, w2, w3)] = np.log(trigram_count[(w1, w2, w3)]) - np.log(skipgram_count[(w1, w3)])
+		skipgram_prob[(w1, w2, w3)] = np.log2(trigram_count[(w1, w2, w3)]) - np.log2(skipgram_count[(w1, w3)])
 	print "calculated skipgram conditional prob ... "
 	return skipgram_prob
 
-# def perplexity(sent, prob):
-# 	for w in sent:
+def get_prob(log_prob_list, word):
+	'''Back-off model, the last one must be unigram model with <UNK> vocab'''
+	for log_prob in log_prob_list:
+		if word in log_prob:
+			return log_prob[word]
+	return log_prob_list[-1]['<UNK>']
 
+def get_log_likelihood(sent, log_prob_list):
+	lld = 0
+	words = sent.split()
+	for w in words:
+		lld += get_prob(log_prob_list, w)
+	return lld / len(words)
 
+def get_perplexity(sent, log_prob_list):
+	lld = get_log_likelihood(sent, log_prob_list)
+	return 2**(-lld)
 
 if __name__ == '__main__':
 	docs, labels = load_data("data/train_text.txt", "data/train_label.txt")
 	unigram_count = get_unigram_count(docs)
 	bigram_count = get_bigram_count(docs)
 	trigram_count = get_trigram_count(docs)
-	unigram_prob = get_unigram_log_prob(unigram_count)
-	bigram_prob = get_bigram_conditional_log_prob(bigram_count, unigram_count)
-	skipgram_prob = get_skipgram_conditional_log_prob(trigram_count)
+	unigram_log_prob = get_unigram_log_prob(unigram_count)
+	bigram_log_prob = get_bigram_conditional_log_prob(bigram_count, unigram_count)
+	skipgram_log_prob = get_skipgram_conditional_log_prob(trigram_count)
+
+	sent = "I HAVE A DREAM"
+	print get_perplexity(sent, [skipgram_log_prob, bigram_log_prob, unigram_log_prob])
